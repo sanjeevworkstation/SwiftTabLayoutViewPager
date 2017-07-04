5333f38ec32c6d38c735562712ac44d9d79c5bfc
Sanjeev Kumar Gautam <sanjeev.gautam@bookmyshow.com>
2017-07-04 11:35:22 +0530

    first commit

---
+++ IOSTablayoutWithViewPager/DisplayContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstViewController.swift
+//  IOSTablayoutWithViewPager
+//
+//  Created by Sanjeev .Gautam on 03/07/17.
+//  Copyright © 2017 SWS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IOSTablayoutWithViewPager/TabLayoutScrollView.swift
@@ -0,0 +1,9 @@
+//
+//  TabLayoutScrollView.swift
+//  IOSTablayoutWithViewPager
+//
+//  Created by Sanjeev .Gautam on 03/07/17.
+//  Copyright © 2017 SWS. All rights reserved.
+//
+
+import Foundation
